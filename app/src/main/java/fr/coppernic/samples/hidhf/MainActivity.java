91ae2688075809ee6d8e515809301ae3b38c686b
Benoist Laforge <benoist.laforge@coppernic.fr>
2018-01-18 11:04:44 +0100

    Finished butterknife integration

--- app/src/main/java/fr/coppernic/samples/hidhf/MainActivity.java
+++ app/src/main/java/fr/coppernic/samples/hidhf/MainActivity.java
@@ -1,9 +1,8 @@
 package fr.coppernic.samples.hidhf;
 
+import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.ScrollingTabContainerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +21,7 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
 import fr.coppernic.sdk.powermgmt.PowerMgmt;
 import fr.coppernic.sdk.powermgmt.PowerMgmtFactory;
@@ -41,206 +41,19 @@ public class MainActivity extends AppCompatActivity implements InstanceListener<
     private static final byte[] SELECT_COMMAND = new byte[]{'s'};
     private static final byte[] CONTINUOUS_MODE_COMMAND = new byte[]{'c'};
     private static final byte[] ABORT_CONTINUOUS_READ_COMMAND = new byte[]{'.'};
-    private static final byte[] ENABLE_ISSO1443A = new byte[]{'o', 'a'};
-
+    // UI
+    @BindView(R.id.swPower)
+    public Switch swPower;
+    @BindView(R.id.swOpen)
+    public Switch swOpen;
+    @BindView(R.id.etCommand)
+    public EditText etCommand;
     // Power Management
     private PowerMgmt powerMgmt;
-
     // Serial port
     private String portName;
     private SerialCom serialCom;
-
-    // UI
-    private Switch swPower;
-    private Switch swOpen;
     private ArrayAdapter<String> adapter;
-    private ArrayAdapter<String> portsAdapter;
-    @BindView(R.id.etCommand) public EditText etCommand;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        ButterKnife.bind(this);
-
-        // Build Power management object
-        powerMgmt = PowerMgmtFactory.get()
-                .setContext(this)
-                .setPeripheralTypes(PeripheralTypesCone.RfidSc)
-                .setManufacturers(ManufacturersCone.Hid)
-                .setModels(ModelsCone.MultiIso)
-                .setInterfaces(InterfacesCone.ExpansionPort)
-                .build();
-
-        // UI
-        swPower = (Switch)findViewById(R.id.swPower);
-        swPower.setOnCheckedChangeListener(onSwPowerCheckedChanged);
-        swOpen = (Switch)findViewById(R.id.swOpen);
-        swOpen.setOnCheckedChangeListener(onSwOpenCheckedChanged);
-        // Firmware version button
-        findViewById(R.id.btnFirmwareVersion).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendCommand(GET_FIRMWARE_COMMAND);
-            }
-        });
-        // Select button
-        findViewById(R.id.btnSelect).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendCommand(SELECT_COMMAND);
-            }
-        });
-
-        // Continuous Mode button
-        findViewById(R.id.btnContinuousMode).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendCommand(CONTINUOUS_MODE_COMMAND);
-            }
-        });
-
-        // Abort continuous mode
-        findViewById(R.id.btnAbortContinuousRead).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendCommand(ABORT_CONTINUOUS_READ_COMMAND);
-            }
-        });
-
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
-        ListView lvLogs = (ListView)findViewById(R.id.lvLogs);
-        lvLogs.setAdapter(adapter);
-
-        // Reader communication
-        SerialFactory.getDirectInstance(this, this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        // Handle presses on the action bar items
-        switch (item.getItemId()) {
-            case R.id.action_clear:
-                adapter.clear();
-                adapter.notifyDataSetChanged();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private CompoundButton.OnCheckedChangeListener onSwPowerCheckedChanged = new CompoundButton.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            if (isChecked) {
-                powerMgmt.powerOn();
-            } else {
-                powerMgmt.powerOff();
-            }
-        }
-    };
-
-    private CompoundButton.OnCheckedChangeListener onSwOpenCheckedChanged = new CompoundButton.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            if (isChecked) {
-                if (serialCom.open(portName, getBaudrate()) != SerialCom.ERROR_OK) {
-                    addLog("Error open com");
-                } else {
-                    serialCom.setRts(true);
-                    addLog("Open com OK");
-                    // Starts listening thread
-                    new Thread(listeningThread).start();
-                    // By default after powered up reader is in continuous reading
-                    // Stops continuous reading
-                    sendCommand(ABORT_CONTINUOUS_READ_COMMAND);
-                }
-            } else {
-                serialCom.close();
-            }
-        }
-    };
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        if (swOpen.isChecked()) {
-            swOpen.setChecked(false);
-        }
-
-        if (swPower.isChecked()) {
-            swPower.setChecked(false);
-        }
-    }
-
-    @Override
-    public void onCreated(SerialCom serialCom) {
-        // Serial instance is obtained
-        this.serialCom = serialCom;
-        String[] devices = this.serialCom.listDevices();
-
-        portName = SERIAL_PORT;
-
-        ArrayList<String> ports = new ArrayList<>();
-        ports.add(SERIAL_PORT);
-
-        for (String s:devices) {
-            if (s.contains("USB")) {
-                ports.add(s);
-            }
-        }
-
-        portsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ports);
-        final Spinner spPorts = (Spinner)findViewById(R.id.spPorts);
-        spPorts.setAdapter(portsAdapter);
-        spPorts.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                portName = spPorts.getSelectedItem().toString();
-                Log.d(TAG, "Selected port: " + portName);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        swOpen.setEnabled(true);
-    }
-
-    @Override
-    public void onDisposed(SerialCom serialCom) {
-
-    }
-
-    /**
-     * Gets baudrate from Spinner
-     * @return Baudrate
-     */
-    private int getBaudrate() {
-        Spinner spBaudrates = (Spinner)findViewById(R.id.spBaudrates);
-        return Integer.parseInt(spBaudrates.getSelectedItem().toString());
-    }
-
-    private void sendCommand(final byte[] command) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                serialCom.send(command, command.length);
-            }
-        }).start();
-    }
-
     /**
      * Listens for serial port data
      */
@@ -269,8 +82,184 @@ public class MainActivity extends AppCompatActivity implements InstanceListener<
         }
     };
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        // Build Power management object
+        powerMgmt = PowerMgmtFactory.get()
+                .setContext(this)
+                .setPeripheralTypes(PeripheralTypesCone.RfidSc)
+                .setManufacturers(ManufacturersCone.Hid)
+                .setModels(ModelsCone.MultiIso)
+                .setInterfaces(InterfacesCone.ExpansionPort)
+                .build();
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
+        ListView lvLogs = (ListView) findViewById(R.id.lvLogs);
+        lvLogs.setAdapter(adapter);
+
+        // Reader communication
+        SerialFactory.getDirectInstance(this, this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_clear:
+                adapter.clear();
+                adapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (swOpen.isChecked()) {
+            swOpen.setChecked(false);
+        }
+
+        if (swPower.isChecked()) {
+            swPower.setChecked(false);
+        }
+    }
+
+    @OnCheckedChanged(R.id.swPower)
+    public void onSwPowerCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            powerMgmt.powerOn();
+        } else {
+            powerMgmt.powerOff();
+        }
+    }
+
+    @OnCheckedChanged(R.id.swOpen)
+    public void onSwOpenCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked) {
+            if (serialCom.open(portName, getBaudrate()) != SerialCom.ERROR_OK) {
+                addLog("Error open com");
+            } else {
+                serialCom.setRts(true);
+                addLog("Open com OK");
+                // Starts listening thread
+                new Thread(listeningThread).start();
+                // By default after powered up reader is in continuous reading
+                // Stops continuous reading
+                sendCommand(ABORT_CONTINUOUS_READ_COMMAND);
+            }
+        } else {
+            serialCom.close();
+        }
+    }
+
+    @OnClick(R.id.btnFirmwareVersion)
+    public void getFirmwareVersion() {
+        sendCommand(GET_FIRMWARE_COMMAND);
+    }
+
+    @OnClick(R.id.btnSelect)
+    public void select() {
+        sendCommand(SELECT_COMMAND);
+    }
+
+    @OnClick(R.id.btnContinuousMode)
+    public void startContinuousMode() {
+        sendCommand(CONTINUOUS_MODE_COMMAND);
+    }
+
+    @OnClick(R.id.btnAbortContinuousRead)
+    public void abortContinuousRead() {
+        sendCommand(ABORT_CONTINUOUS_READ_COMMAND);
+    }
+
+    @OnClick(R.id.btnSend)
+    public void sendCustomCommand() {
+        String command = etCommand.getText().toString();
+        sendCommand(command.getBytes());
+    }
+
+    @Override
+    public void onCreated(SerialCom serialCom) {
+        // Serial instance is obtained
+        this.serialCom = serialCom;
+        String[] devices = this.serialCom.listDevices();
+
+        portName = SERIAL_PORT;
+
+        ArrayList<String> ports = new ArrayList<>();
+        ports.add(SERIAL_PORT);
+
+        for (String s : devices) {
+            if (s.contains("USB")) {
+                ports.add(s);
+            }
+        }
+
+        ArrayAdapter<String> portsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, ports);
+        final Spinner spPorts = (Spinner) findViewById(R.id.spPorts);
+        spPorts.setAdapter(portsAdapter);
+        spPorts.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                portName = spPorts.getSelectedItem().toString();
+                Log.d(TAG, "Selected port: " + portName);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        swOpen.setEnabled(true);
+    }
+
+    @Override
+    public void onDisposed(SerialCom serialCom) {
+
+    }
+
+    /**
+     * Gets baudrate from Spinner
+     *
+     * @return Baudrate
+     */
+    private int getBaudrate() {
+        Spinner spBaudrates = (Spinner) findViewById(R.id.spBaudrates);
+        return Integer.parseInt(spBaudrates.getSelectedItem().toString());
+    }
+
+    /**
+     * Sends a command to serial port
+     * @param command Byte array command to be sent
+     */
+    private void sendCommand(final byte[] command) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                serialCom.send(command, command.length);
+            }
+        }).start();
+    }
+
     /**
      * Displays a log message in the logs list view
+     *
      * @param s Message to be displayed
      */
     private void addLog(final String s) {
@@ -278,17 +267,11 @@ public class MainActivity extends AppCompatActivity implements InstanceListener<
             @Override
             public void run() {
                 String[] str = s.split("\n");
-                for (String s:str) {
+                for (String s : str) {
                     adapter.insert(s, 0);
                 }
                 adapter.notifyDataSetChanged();
             }
         });
-    }
-
-    @OnClick(R.id.btnSend)
-    public void sendCustomCommand() {
-        String command = etCommand.getText().toString();
-        sendCommand(command.getBytes());
     }
 }
